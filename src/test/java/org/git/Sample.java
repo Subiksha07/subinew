12c4357786beb9b045180cb5e80962492c779ad2
Subiksha07 <ravi.subiksha@gmail.com>
2021-01-18 19:02:40 +0530

    Created

---
+++ src/test/java/org/git/Sample.java
@@ -0,0 +1,14 @@
+package org.git;
+
+public class Sample {
+	
+	public static void method() {
+
+		System.out.println("Hai World");
+	}
+
+	public static void main(String[] args) {
+		
+		method();
+	}
+}
